100d515b8ffa167c08b16bddd0d3c59d8e9e65b4
Elena Troeglazova <BadIllusion@yandex.ru>
2026-09-02 03:29:47 +0300

    Исправить ошибки из code check

--- src/test/java/ru/yandex/practicum/filmorate/BaseTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/BaseTest.java
@@ -1,13 +1,11 @@
 package ru.yandex.practicum.filmorate;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.junit.jupiter.api.BeforeAll;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 
-import java.io.IOException;
 import java.net.http.HttpClient;
 import java.time.Duration;
 
